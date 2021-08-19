c788f17299bd8dabd142997a61b9317962fe58c6
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-19 23:54:02 +0100

    generate app logic

--- core/generators.py
+++ core/generators.py
@@ -1,20 +1,27 @@
 from django.conf import settings
+from os import path, system
 
 class Generator(object):
 
-    def __init__(self, app_name, model_name, fields):
-        self.app_name = app_name
+    def __init__(self, appdir, model_name, fields):
+        self.appdir = appdir
+        self.MAIN_DIR = appdir.split("/", maxsplit=1)[0]
+        self.app_name = appdir.split("/", maxsplit=1)[1]
         self.model_name = model_name
         self.fields = fields
-        self.MAIN_DIR = './'
 
     def generate(self):
         self.generate_app()
         self.generate_model()
-        print("finished %s %s %s"% (self.app_name, self.model_name, self.fields))
+        print("finished %s %s %s %s"% (self.MAIN_DIR, self.app_name, self.model_name, self.fields))
 
     def generate_app(self):
-        print("Generated App")
+        if not path.exists('%s' % (self.appdir)):
+            system('python manage.py startapp %s' % self.app_name)
+            system('mv %s %s' % (self.app_name, self.appdir))
+            print("Generated App")
+        else:
+            print("app already exist @ %s" % (self.appdir))
 
     def generate_model(self):
-        print("Generated Model")
+        print("Generated Model")     
