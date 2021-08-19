572a3c98afb138f0d8c23a834724ea7578cbbc29
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-19 22:11:03 +0100

    missing arguments error message

--- core/management/commands/drf_scaffold.py
+++ core/management/commands/drf_scaffold.py
@@ -2,13 +2,16 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = 'Meant to generate Models/Views/Urls/Serializers'
+    missing_args_message = (
+        "You are missing some arguments in your command check the example below"
+        "python manage.py drf_scaffold blog Article title:char body:text"
+    )
 
     def add_arguments(self, parser):
+        #need to run command as follow: python manage.py drf_scaffold app_name ModelName fields
         parser.add_argument(
-            'app',
-            help='app name',
+            'args', metavar='scaffold', nargs='*', help='Scaffold arguments (app_name ModelName fields).'
         )
 
     def handle(self, *args, **kwargs):
-        app_name = kwargs['app']
-        print(app_name)
+        print(args)
