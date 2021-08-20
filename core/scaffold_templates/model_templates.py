72fa7f4817ef47c242673a119ed366c11af55b95
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-20 16:31:18 +0100

    reafoctored model templates

---
+++ core/scaffold_templates/model_templates.py
@@ -0,0 +1,28 @@
+MODEL = """class %s(models.Model):\
+    %s
+    create_date = models.DateTimeField(auto_now_add=True)
+    
+"""
+MODEL_IMPORT = """from %(app)s.models import %(model)s
+"""
+CHARFIELD = """
+    %(name)s = models.CharField(max_length=255, null=True, blank=True)"""
+TEXTFIELD = """
+    %(name)s = models.TextField(null=True, blank=True)"""
+INTEGERFIELD = """
+    %(name)s = models.IntegerField(null=True, default=0)"""
+DECIMALFIELD = """
+    %(name)s = models.DecimalField(null=True, default=0.0)"""
+DATETIMEFIELD = """
+    %(name)s = models.DateTimeField(null=True)"""
+FOREIGNKEY = """
+    %(name)s = models.ForeignKey(%(foreign)s, null=True, blank=True)"""
+
+FIELD_TYPES = {
+    'charfield': CHARFIELD,
+    'textfield': TEXTFIELD,
+    'integerfield':INTEGERFIELD,
+    'decimalfield':DECIMALFIELD,
+    'datetimefield':DATETIMEFIELD,
+    'foreignkey':FOREIGNKEY,
+}
