f829dad59a54ac89c53adff14c6574f6a7e1f3ef
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-22 14:57:28 +0100

    refactoring and some code documentation

--- drf_scaffold_core/generators.py
+++ drf_scaffold_core/generators.py
@@ -22,8 +22,8 @@ class Generator(object):
       self.MAIN_DIR = './'
       self.app_name = appdir
       if len(appdir.split('/'))>= 2:
-        self.MAIN_DIR = appdir.split("/", maxsplit=1)[0]
-        self.app_name = appdir.split("/", maxsplit=1)[1]
+        self.MAIN_DIR = appdir.split("/")[0]
+        self.app_name = appdir.split("/")[1]
       self.model_name = model_name
       self.fields = fields
 
@@ -31,9 +31,13 @@ class Generator(object):
       self.generate_app()
       self.generate_models()
       self.register_models_to_admin()
-      # self.generate_serializers()
-      # self.generate_views()
-      # self.generate_urls()
+      self.generate_serializers()
+      self.generate_views()
+      self.generate_urls()
+
+    def add_setup_imports(self, file_paths, imports):
+      for i, f in enumerate(file_paths):
+        file_api.set_file_content(f, imports[i])
 
     def setup_files(self):
       extra_files = (f"{self.appdir}/serializers.py", f"{self.appdir}/urls.py" )
@@ -44,10 +48,6 @@ class Generator(object):
       wipe_files(all_files)
       self.add_setup_imports(all_files, setup_imports)
 
-    def add_setup_imports(self, file_paths, imports):
-      for i, f in enumerate(file_paths):
-        file_api.set_file_content(f, imports[i])
-
     def generate_app(self):
       if not path.exists('%s' % (self.appdir)):
         system(f'python manage.py startapp {self.app_name}')
@@ -56,101 +56,119 @@ class Generator(object):
       else:
         print(f"App does already exist at {self.appdir}")
 
-    def get_model_string(self):
-      fields_list = self.get_fields_template_list(self.fields)
-      fields_string = ''.join(field for field in fields_list)
-      verbose_model = pluralize(self.model_name).capitalize()
-      model_string = model_templates.MODEL % (self.model_name, fields_string, verbose_model)
-      return model_string
-
-    def generate_models(self):
-      models_file = f"{self.appdir}/models.py"
-      model_class_head = f'class {self.model_name}'
-      if file_api.is_present_in_file(models_file, model_class_head):
-        return 
-      model_string = self.get_model_string()
-      file_api.append_file_content(models_file, model_string)
-
-    def rewrite_component_file(self, file_path, head, body):
-      with open(file_path, 'r+') as file:
-        file_content = ''.join(line for line in file.readlines())
-        if file_path == f"{self.appdir}/urls.py":
-          file_content = file_content.replace(url_templates.URL_PATTERNS,"")
-        new_content = head + file_content + body + "\n"
-        file.seek(0)
-        file.write(new_content)
-
-    def get_fields_template_list(self, fields):
+    """ 
+    MODELS GENERATION METHODS 
+        1 - get_fields_string : match fields from cli with their templates and return a string of them joined
+        2 - get_model_string : yield the fields string in the Model template and returns a new Model string
+        3 - generate_models: check if the model does already exists in the models.py file if not it append it
+    """
+    def get_fields_string(self, fields):
       actual_fields = list()
       for field in fields:
-        new_field = self.select_field_template(field)
-        if new_field:
-          actual_fields.append(new_field)
-      return actual_fields
+        field_name = field.split(':')[0]
+        field_type = field.split(':')[1].lower()
+        field_dict = dict(name= field_name, foreign = field.split(':')[2] if (field_type == 'foreignkey') else '')
+        field_template = model_templates.FIELD_TYPES[field_type] % field_dict
+        actual_fields.append(field_template)
+      fields_string = ''.join(f for f in actual_fields)    
+      return fields_string
 
-    def class_exist(self, component, file, model): 
-      for line in file.readlines():
-        if component == 'view':
-          if f'class {model}ViewSet' in line:
-            print(f'ViewSet already exists at {self.appdir}/views.py')
-            return True   
-        elif component == 'serializer':
-          if f'class {model}Serializer' in line:
-            print(f'Serializer already exists at {self.appdir}/serializers.py')
-            return True
-        elif component == 'url':
-          if f'{model}ViewSet)' in line:
-            print(f'Url already exists at {self.appdir}/urls.py')
-            return True           
-      return False
+    def get_model_string(self):
+      fields_string = self.get_fields_string(self.fields)
+      params = (self.model_name, fields_string, pluralize(self.model_name).capitalize())
+      return model_templates.MODEL % params
 
-    def select_field_template(self, field):
-      field_name = field.split(':')[0]
-      field_type = field.split(':')[1].lower()
-      return model_templates.FIELD_TYPES[field_type] % dict(name= field_name, foreign = field.split(':')[2] if (field_type == 'foreignkey') else '')
-
-    def is_imported(self, path, model):
-      file = open(path, 'r')
-      for line in file.readlines():
-        if f'import {model}' in line:
-          return True
-      return False
-
-    def register_models_to_admin(self):
-      admin_file = f"{self.appdir}/admin.py"
-      admin_register_head = f'@admin.register({self.model_name})'
-      if file_api.is_present_in_file(admin_file, admin_register_head):
+    def generate_models(self):
+      file = f"{self.appdir}/models.py"
+      chunk = f'class {self.model_name}'
+      if file_api.is_present_in_file(file, chunk):
         return 
+      model_string = self.get_model_string()
+      file_api.append_file_content(file, model_string)
+
+    """ 
+    MODELS REGISTRATION TO ADMIN 
+        1 - get_admin_parts : returns the register template and imports of the Model class
+        2 - register_models_to_admin : check if the model already registered in admin.py if not 
+        it wraps the admin file content between the Model imports and register template 
+    """
+    def get_admin_parts(self):
       app_path = self.appdir.replace("/", ".")
       model_register_template = admin_templates.REGISTER % {'model': self.model_name}
       model_import_template = admin_templates.MODEL_IMPORT % {'app': app_path, 'model': self.model_name}
-      file_api.wrap_file_content(admin_file, model_import_template, model_register_template)
+      return (model_import_template, model_register_template)
+
+    def register_models_to_admin(self):
+      file = f"{self.appdir}/admin.py"
+      chunk = f'@admin.register({self.model_name})'
+      if file_api.is_present_in_file(file, chunk):
+        return 
+      head, body = self.get_admin_parts()
+      file_api.wrap_file_content(file, head, body)
+
+    """ 
+    VIEWS GENERATION 
+        1 - get_viewset_parts : returns the viewset template and model + serializers imports of the Model class
+        2 - generate_views : check if the viewset already exists in views.py if not 
+        it wraps the file content between the imports and the viewset template 
+    """
+    def get_viewset_parts(self):
+      app_path = self.appdir.replace("/", ".")
+      viewset_template = view_templates.VIEWSET % {'model': self.model_name}
+      model_import_template = view_templates.MODEL_IMPORT % {'app': app_path, 'model': self.model_name}
+      serializer_import_template= view_templates.SERIALIZER_IMPORT % {'app': app_path, 'model': self.model_name}
+      imports = model_import_template + serializer_import_template
+      return (imports, viewset_template)
 
     def generate_views(self):
-      view_file = open(f"{self.appdir}/views.py", 'r')
-      if self.class_exist('view', view_file, self.model_name):
+      file = f"{self.appdir}/views.py"
+      chunk = f'class {self.model_name}ViewSet'
+      if file_api.is_present_in_file(file, chunk):
         return 
-      viewset_template = view_templates.VIEWSET % {'model': self.model_name}
-      model_import_template = view_templates.MODEL_IMPORT % {'app': self.appdir.replace("/", "."), 'model': self.model_name}
-      serializer_import_template= view_templates.SERIALIZER_IMPORT % {'app': self.appdir.replace("/", "."), 'model': self.model_name}
-      imports = model_import_template+serializer_import_template
-      self.rewrite_component_file(f"{self.appdir}/views.py", imports,viewset_template)
-      return print(f"🚀 {self.appdir}/views.py have been successfully updated")
+      head, body = self.get_viewset_parts()  
+      file_api.wrap_file_content(file, head, body)
+    
+    """ 
+    SERIALIZER GENERATION 
+        1 - get_serializer_parts : returns the serializer template and imports of the Model class
+        2 - generate_serializers : check if the viewset already exists in serializers.py if not 
+        it wraps the file content between the imports and the serializer template 
+    """
+    def get_serializer_parts(self):
+      app_path = self.appdir.replace("/", ".")
+      serializer_template = serializer_templates.SERIALIZER % {'model': self.model_name}
+      imports = serializer_templates.MODEL_IMPORT % {'app': app_path, 'model': self.model_name}
+      return(imports, serializer_template)
 
     def generate_serializers(self):
-      serializer_file = open(f"{self.appdir}/serializers.py", 'r')
-      if self.class_exist('serializer', serializer_file, self.model_name):
+      serializer_file = f"{self.appdir}/serializers.py"
+      serializer_head = f'class {self.model_name}Serializer'
+      if file_api.is_present_in_file(serializer_file, serializer_head):
         return 
-      serializer_template = serializer_templates.SERIALIZER % {'model': self.model_name}
-      model_import_template = serializer_templates.MODEL_IMPORT % {'app': self.appdir.replace("/", "."), 'model': self.model_name}
-      self.rewrite_component_file(f"{self.appdir}/serializers.py", model_import_template,serializer_template)
-      return print(f"🚀 {self.appdir}/views.py have been successfully updated")
+      head, body = self.get_serializer_parts()  
+      file_api.wrap_file_content(serializer_file, head, body)
+    
+    """ 
+    URLS GENERATION 
+        1 - get_url_parts : returns the url template and model imports of the Model class
+        2 - generate_urls : check if the url already exists in urls.py if not 
+        it wraps the file content between the imports and the url template, before wrapping 
+        it checks if the file has URL_PATTERS it take it off and append it to the url template 
+    """
+    def get_url_parts(self):
+      app_path = self.appdir.replace("/", ".")  
+      plural_path = pluralize(self.model_name.lower()).capitalize()
+      url_template = url_templates.URL % {'model': self.model_name, 'path': plural_path}
+      imports = url_templates.MODEL_IMPORT % {'app': app_path, 'model': self.model_name}
+      return(imports, url_template)
 
     def generate_urls(self):
-      urls_file = open(f"{self.appdir}/urls.py", 'r')
-      if self.class_exist('url', urls_file, self.model_name):
+      file = f"{self.appdir}/urls.py"
+      chunk = f'{self.model_name}ViewSet)'
+      if file_api.is_present_in_file(file, chunk):
         return 
-      url_template = url_templates.URL % {'model': self.model_name, 'path': pluralize(self.model_name.lower())} + url_templates.URL_PATTERNS
-      model_import_template = url_templates.MODEL_IMPORT % {'app': self.appdir.replace("/", "."), 'model': self.model_name}
-      self.rewrite_component_file(f"{self.appdir}/urls.py", model_import_template,url_template)
-      return print(f"🚀 {self.appdir}/urls.py have been successfully updated")
+      head, body = self.get_url_parts() 
+      if file_api.is_present_in_file(file, url_templates.URL_PATTERNS):
+        file_api.replace_file_chunk(file,url_templates.URL_PATTERNS, "")
+      body = body + url_templates.URL_PATTERNS
+      file_api.wrap_file_content(file, head, body)
--- drf_scaffold_core/scaffold_templates/serializer_templates.py
+++ drf_scaffold_core/scaffold_templates/serializer_templates.py
@@ -3,12 +3,10 @@ SERIALIZER = """class %(model)sSerializer(serializers.HyperlinkedModelSerializer
         model = %(model)s
         fields = '__all__'
         
+
 """
 
 MODEL_IMPORT = """from %(app)s.models import %(model)s
-"""
-
-SERIALIZER_SETUP = """from rest_framework import serializers
 """
 
 SETUP="""from rest_framework import serializers
