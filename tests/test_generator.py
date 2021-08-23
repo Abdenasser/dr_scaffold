a64c32aae8a582669e90c5d37c433105b8964245
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-24 00:39:33 +0100

    some tests for generator

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,13 +1,14 @@
 import os
 import pytest  
 import tempfile  
-from os import path
+from os import path, system
 from dr_scaffold.generators import Generator
 from dr_scaffold.scaffold_templates import serializer_templates, model_templates
 from unittest import TestCase
 
 class TestGenerator(TestCase):
     tmpfilepath = os.path.join(tempfile.gettempdir(), "tmp-testfile")
+    tmpdirpath = tempfile.mkdtemp()
     generator = Generator("apps/blog", "Article", ("title:charfield", "body:textfield"))
 
     def setUp(self):
@@ -32,6 +33,17 @@ class TestGenerator(TestCase):
             body = ''.join(line for line in file.readlines())
         assert body == serializer_templates.SETUP
 
+    def test_setup_files(self):
+        g = Generator(f"{self.tmpdirpath}/blog", "Article", ("title:charfield", "body:textfield"))
+        system(f'python manage.py startapp {g.app_name}')
+        system(f'mv {g.app_name} {self.tmpdirpath}')
+        #making the appdir the temp folder for test purpose
+        g.appdir = self.tmpdirpath
+        g.setup_files()
+        files = [f for f in os.listdir(self.tmpdirpath)]        
+        assert len(files) == 5
+        assert files == ['models.py', 'serializers.py', 'admin.py', 'urls.py', 'views.py']
+
     def test_get_fields_string(self):
         g = self.generator
         fields_string = g.get_fields_string(g.fields)
@@ -44,3 +56,19 @@ class TestGenerator(TestCase):
         model_string = g.get_model_string()
         string = model_templates.MODEL % ("Article", fields_string, "Articles")
         assert model_string == string
+
+    def test_generate_models(self):
+        g = Generator(f"{self.tmpdirpath}/blog", "Article", ("title:charfield", "body:textfield"))
+        system(f'python manage.py startapp {g.app_name}')
+        system(f'mv {g.app_name} {self.tmpdirpath}')
+        #making the appdir the temp folder for test purpose
+        g.appdir = self.tmpdirpath
+        g.setup_files()
+        g.generate_models()      
+        with open(f"{g.appdir}/models.py", 'r+') as file:
+            body = ''.join(line for line in file.readlines())
+        assert ('Article' in body) == True
+        assert ('Articles' in body) == True
+        assert ('title' in body) == True
+        assert ('body' in body) == True
+        
