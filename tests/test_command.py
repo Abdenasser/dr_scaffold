6ac037cf3a76fe57cb7833031fcfe725c54b86b5
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-24 13:37:53 +0100

    test coverage

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,11 +1,8 @@
-
-
 import django
 from django.conf import settings
 from django.core.management import call_command
 from django.test import override_settings
 from django.test.testcases import SimpleTestCase
-from dr_scaffold.generators import Generator
 from unittest import mock
 
 settings.configure()
@@ -18,15 +15,6 @@ django.setup()
 )
 
 class CommandTestCase(SimpleTestCase):
-    @mock.patch('dr_scaffold.management.commands.dr_scaffold.Command.add_arguments')
-    def test_add_arguments(self, mock_add_arguments):
-        call_command('dr_scaffold', 'blog', 'Article', 'title:charfield')
-        mock_add_arguments.assert_called()
-
-    @mock.patch('dr_scaffold.management.commands.dr_scaffold.Command.handle')
-    def test_add_arguments(self, mock_handle):
-        call_command('dr_scaffold', 'blog', 'Article', 'title:charfield')
-        mock_handle.assert_called()
 
     @mock.patch('dr_scaffold.generators.Generator.run')
     def test_command(self, mock_run):
