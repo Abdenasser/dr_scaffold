b091ee897740d7a11408eb430b7d429650d07b56
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-28 18:33:20 +0100

    handle missing settings and forward slashes

--- Scripts/check.sh
+++ Scripts/check.sh
@@ -6,4 +6,4 @@ set -x
 
 flake8 $SOURCE_FILES --ignore=E501,E722 --exclude=__init__.py
 black --check --diff --target-version=py38 $SOURCE_FILES
-mypy $SOURCE_FILES --no-site-packages
+mypy $SOURCE_FILES
