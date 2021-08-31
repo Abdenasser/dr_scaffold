c92607c12478e0004b5b0fc1d214579e70a61e22
Regis Santos <regis42santos@gmail.com>
2021-08-31 02:26:35 -0300

    lint: remove white space on admin_templates.

--- dr_scaffold/scaffold_templates/admin_templates.py
+++ dr_scaffold/scaffold_templates/admin_templates.py
@@ -5,7 +5,7 @@ templates for admin
 REGISTER = """@admin.register(%(model)s)
 class %(model)sAdmin(admin.ModelAdmin):
     exclude = ()
-    
+
 """
 MODEL_IMPORT = """from %(app)s.models import %(model)s
 """
