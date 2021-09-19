76453cd1a97cc55af5026b8ef7eb9122333abbb9
Abdenasser <nasser.elidrissi065@gmail.com>
2021-09-19 13:17:36 +0100

    added *model representation string

--- dr_scaffold/scaffold_templates/model_templates.py
+++ dr_scaffold/scaffold_templates/model_templates.py
@@ -6,6 +6,9 @@ MODEL = """class %s(models.Model):\
     %s
     create_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"%s"
+
     class Meta:
         verbose_name_plural = "%s"
 
