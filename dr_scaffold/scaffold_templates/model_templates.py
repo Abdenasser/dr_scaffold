b709c13fac5aae4fca8d1a285d7cd591f2b9f5d7
Regis Santos <regis42santos@gmail.com>
2022-01-16 05:37:12 -0300

    refactor: rename create_date field to created. close #51

--- dr_scaffold/scaffold_templates/model_templates.py
+++ dr_scaffold/scaffold_templates/model_templates.py
@@ -4,7 +4,7 @@ templates for models
 
 MODEL = """class %s(models.Model):\
     %s
-    create_date = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"%s"
